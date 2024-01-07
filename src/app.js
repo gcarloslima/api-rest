cb18b06e396fb2ad04791316ea7abef1d62723a9
gcarloslima <carlosrelgs@gmail.com>
2024-01-07 17:53:19 -0300

    Separando o código em diferentes arquivos usando ECMAScript modules.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 // Também instalei o nodemon, para as atualizações no código refletirem em tempo real no servidor.
 
-const express = require("express"); // importar o express
+import express from "express"; // importar o express
 const app = express(); // criar uma instância do express
-const port = 3000; // definir a porta
+
 
 // Definir a rota padrão ou raiz
 app.get('/', (req, res) => {
     res.send("Hello World, Carlos!");
 });
 
-// escutar as requisições para a porta 3000
-app.listen(port, () => {
-    console.log(`O servidor está rodando no endereço http:/localhost:${port}`);
-});
+export default app;
