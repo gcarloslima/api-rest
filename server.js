93e2487bbed34a77023eb287bdf9ff724f06e7a9
gcarloslima <carlosrelgs@gmail.com>
2024-01-07 21:00:01 -0300

    listar dados mockados

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import app from "./src/app.js"; // importar app
 
-const port = 3000; // definir a porta
+const PORT = 3000; // definir a porta
 
 // escutar as requisições para a porta 3000
-app.listen(port, () => {
-    console.log(`O servidor está rodando no endereço http:/localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`O servidor está rodando no endereço http:/localhost:${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -4,9 +4,26 @@ import express from "express"; // importar o express
 const app = express(); // criar uma instância do express
 
 
+// mock
+const livros = [
+    { id: 1, titulo: "Clean Code: A Handbook of Agile Software Craftsmanship", dataPublicacao: "11 de agosto de 2008"},
+    { id: 2, titulo: "Refactoring: Improving the Design of Existing Code (2nd Edition)", dataPublicacao: "30 de novembro de 2018"},
+    { id: 3, titulo: "The Clean Coder: A Code of Conduct for Professional Programmers", dataPublicacao: "23 de maio de 2011" },
+    { id: 4, titulo: "Domain-driven design: atacando as complexidades no coração do software", dataPublicacao: "16 de dezembro de 2016" },
+    { id: 5, titulo: "Clean Architecture: A Craftsman's Guide to Software Structure and Design", dataPublicacao: "10 de setembro de 2017"},
+    { id: 6, titulo: "Learning JavaScript Data Structures and Algorithms: Write complex and powerful JavaScript code using the latest ECMAScript, 3rd Edition", dataPublicacao: "30 de abril de 2018"}
+];
+
+
 // Definir a rota padrão ou raiz
 app.get('/', (req, res) => {
-    res.send("Hello World, Carlos!");
+    res.send("Hello World, Carlos Rangel!");
 });
+
+app.get('/livros', (req, res) => {
+    res.status(200).send(livros);
+});
+
+
 
 export default app;
